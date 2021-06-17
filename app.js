0e31cd7012c1258e03c0a17d6a293eff9954b957
Hammurabi <koberstine@hotmail.com>
2021-06-17 16:59:29 -0400

    installed inquirer

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
+const inquirer = require('inquirer');
+/*
 const fs = require('fs');
 const generatePage = require('./src/page-template');
-
-const profileDataArgs = process.argv.slice(2);
-
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-
-console.log(name, github);
 
 const pageHTML = generatePage(name, github);
 
@@ -16,3 +10,13 @@ fs.writeFile('./index.html', pageHTML, err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
+*/
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
